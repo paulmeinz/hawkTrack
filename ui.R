9eb988ab67d1e2e396652d83392b2cdedaf2fdee
paulmeinz <paulmeinz@gmail.com>
2018-05-28 14:13:46 -0700

    ADDED TABS FOR THE COHORT TRACKER

--- ui.R
+++ ui.R
@@ -1,33 +1,31 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(shiny)
+library(dplyr)
+library(rCharts)
+library(tidyr)
+library(shinyjs)
+library(shinyBS)
 
-# Define UI for application that draws a histogram
+
+################################################################################
+
+#                                  DEFINE UI
+
+################################################################################
+
+
 shinyUI(fluidPage(
+  useShinyjs(),
   
   # Application title
-  titlePanel("Old Faithful Geyser Data"),
-  
-  # Sidebar with a slider input for number of bins 
-  sidebarLayout(
-    sidebarPanel(
-       sliderInput("bins",
-                   "Number of bins:",
-                   min = 1,
-                   max = 50,
-                   value = 30)
-    ),
-    
-    # Show a plot of the generated distribution
-    mainPanel(
-       plotOutput("distPlot")
-    )
+  navbarPage(title = 'CRC HawkTrack',
+     tabPanel(title = 'Select a Cohort'),
+     tabPanel(title = 'Cohort Profile'),
+     tabPanel(title = 'Cohort Achievements')
   )
+
+    
 ))
+             
+  
+  
