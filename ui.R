bc0f4bf3c44d3395be90721258b1d875a3305a0b
Paul Meinz <paulmeinz@gmail.com>
2018-05-30 10:09:23 -0700

    FIXED YEAR ORDER CHANGED CSS FOR COHORT MESSAGE

--- ui.R
+++ ui.R
@@ -19,6 +19,7 @@ crcLink <- "https://researchapps.crc.losrios.edu/CRC_Research_Data_Request_Form"
 
 # Cohort Selection--------------------------------------------------------------
 cohort <- unique(cohorts$acad_year[cohorts$term == 1])
+cohort <- cohort[order(cohort, decreasing = TRUE)]
 definition <- c('All', 'Two Year Path', 'Three Year Path', 
                 'Degree/Transfer/Certificate Seeking')
 
